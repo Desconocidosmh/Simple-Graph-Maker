6b9c86fc82dfb5ce3abc556ab5a30a89b440c09a
Desconocidosmh <czerwonapanda1@interia.pl>
2018-12-16 14:09:44 +0100

    Add cubic function

---
+++ Graph/Elements/CubicFunction.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Graph.System;
+
+namespace Graph.Elements
+{
+    [MenuClass("Cubic Function")]
+    public class CubicFunction : Element
+    {
+        [MenuProperty("A")]
+        public float A { get; set; }
+        [MenuProperty("B")]
+        public float B { get; set; }
+        [MenuProperty("C")]
+        public float C { get; set; }
+        [MenuProperty("D")]
+        public float D { get; set; }
+
+        public override float Calculate(float x) =>
+            (float)(A * Math.Pow(x, 3) + B * Math.Pow(x, 2) + C * x + D);   
+    }
+}
